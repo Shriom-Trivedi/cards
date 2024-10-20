ade974d45534c0d40e6df559dd3eec9605371cf7
Shri Om Trivedi <tshriom@gmail.com>
2024-10-20 18:26:59 +0530

    deck.toString() method added

--- main.go
+++ main.go
@@ -5,7 +5,5 @@ import "fmt"
 func main() {
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 3)
-
-	fmt.Println("hand:", hand, "\nremainingDeck", remainingDeck)
+	fmt.Println(cards.toString())
 }
